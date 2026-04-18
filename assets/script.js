05b0630995bb430025912a5c802f3fb335c35325
syncinggg <kingchristsar@gmail.com>
2026-04-19 04:22:08 +0800

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,47 @@
+document.querySelectorAll(".nav-link").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const target = document.querySelector(link.getAttribute("href"));
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+});
+
+
+const container = document.querySelector(".card-content");
+
+let isScrolling;
+
+container.addEventListener("scroll", () => {
+  window.clearTimeout(isScrolling);
+
+  isScrolling = setTimeout(() => {
+    const sections = document.querySelectorAll(".snap");
+    const scrollPos = container.scrollTop;
+
+    let closest = null;
+    let closestDistance = Infinity;
+
+    sections.forEach(section => {
+      const offset = section.offsetTop;
+      const distance = Math.abs(offset - scrollPos);
+
+      if (distance < closestDistance) {
+        closestDistance = distance;
+        closest = section;
+      }
+    });
+
+    if (closest) {
+      closest.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+
+  }, 80);
+});
